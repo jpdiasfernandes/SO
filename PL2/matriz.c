4de877759caf564ccdb7964aa6ac93217f9c4e48
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2021-03-18 20:38:11 +0000

    Add formatting and delete deprecated comments

--- PL2/matriz.c
+++ PL2/matriz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <string.h> /* strcmp */
-#include <stdlib.h> /* rand and atoi */
-#include <unistd.h> /* essential defs and decls */
+#include <stdlib.h>   /* rand and atoi */
+#include <string.h>   /* strcmp */
 #include <sys/wait.h> /* wait and macros */
+#include <time.h>     /* to seed random function */
+#include <unistd.h>   /* essential defs and decls */
 
 #define MAX_COL 4096
 #define MAX_LINE 128
+#define MAX_RAND 530000
 
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	// To run through the matrix
 	int i, j;
 	// To get store the pid
@@ -21,51 +22,50 @@ int main (int argc, char *argv[]) {
 	// Stores status from child process
 	int status;
 
+	if (argc > 2) {
+		perror("args: matriz number");
+		exit(1);
+	}
+
 	int **mat = malloc(MAX_LINE * sizeof(char *));
 
-	if (argc > 2) { perror("args: matriz number"); exit(1);}
+	// Seeds random function with current time
+	srandom(time(NULL));
 
 	arg = atoi(argv[1]);
 
-	// O  meu raciocínio é que como o fork duplica todos os registos
-	// também vai duplicar o pc. Deste modo só depois do fork é que 
-	// o código vai ser executado. O que faz sentido, não vemos a 
-	// main a ser executada mais que uma vez nos exemplos.
-	
 	for (i = 0; i < MAX_LINE; i++) {
 		mat[i] = malloc(sizeof(int) * MAX_COL);
 		for (j = 0; j < MAX_COL; j++) {
-			// Although rand is not the most reliant peseudo-random
-			// algorithm for the sake of an exercise like this
-			// no need to use POSIX compliant functions.
-			int rnd = random();	
-			// Gets a random number between 0 and 250001
-			mat[i][j] = rnd % 530001;
+			int rnd = random();
+			// Gets a random number between 0 and MAX_RAND
+			mat[i][j] = rnd % (MAX_RAND + 1);
 		}
-	}	
-	for(i = 0; i < MAX_LINE; i++) 
-		mat[i][i] = i;
-	
+	}
+
+	for (i = 0; i < MAX_LINE; i++) mat[i][i] = i;
+
+	// Creates MAX_LINE childs from the first parent
 	for (i = 0; i < MAX_LINE && pid != 0; i++) {
-		if ( (pid = fork()) == -1 ) { perror("Unable to fork."); exit(1);}
+		if ((pid = fork()) == -1) {
+			perror("Unable to fork.");
+			exit(1);
+		}
 	}
 	// Child executing
 	if (pid == 0) {
-		for (j = 0; j < MAX_COL; j++) 
+		for (j = 0; j < MAX_COL; j++)
 			if (mat[i][j] == arg) exit(i);
-		exit(0);
-	}
-	else {
+		_exit(0);
+	} else {
 		for (i = 0; i < MAX_LINE; i++) {
 			wait(&status);
 			if (r == 0) r = WEXITSTATUS(status);
 		}
 
 		if (r) printf("Found the number in line %d", r);
-
 	}
 
 	return 0;
 }
-
 
